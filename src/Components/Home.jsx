4947b41b487d6ba5868e4d5cf910b536c664ba1c
Abram Mina <114501769+Abram122@users.noreply.github.com>
2024-06-10 00:28:54 +0200

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -78,8 +78,8 @@ const Home = () => {
     return (
         <div className="bg-black m py-4">
             <div className="container bg-black text-white m-auto">
-                <h1 className="text-center text-3xl">Welcome to Abram's Website for serching <br /> and store your fav film to see at any time</h1>
-                <form action="">
+                <h1 className="text-center text-3xl">Welcome to my Website for serching <br /> and store your fav film to see at any time</h1>
+                <form action="" className="w-[90%] m-auto">
                     <input type="text" placeholder="Enter the film Name" className=" text-black mt-4 peer w-full h-full  text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0  disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900" onChange={(e) => {
                         setSearch(e.target.value)
                     }} />
@@ -95,7 +95,7 @@ const Home = () => {
                             <h1>Realease Time: {data.Released}</h1>
                             <h1>Time: {data.Runtime}</h1>
                             <h1>Type: {data.Type}</h1>
-                            <h1 className="lg:text-wrap xlg:w-[600px]">Story: {data.Plot}</h1>
+                            <h1 className=" text-wrap lg:text-wrap xlg:w-[600px]">Story: {data.Plot}</h1>
                             <div className="text-center">
                                 <button onClick={() => {
                                     store(data.Poster, data.Title, data.Released)
