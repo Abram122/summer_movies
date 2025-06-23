5bf34621a67d7980b98481e4ac3e8a12ae326755
Abram Mina <114501769+Abram122@users.noreply.github.com>
2025-06-23 12:58:35 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./Pages/Home"
 import Movies from "./Pages/Movies"
-import Loader from "./components/loader"
 import MovieDetails from "./Pages/MovieDetails"
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
       <Route path="/" element={<Home/>}/>
       <Route path="/movies" element={<Movies/>}/>
       <Route path="/movies/:id" element={<MovieDetails/>}/>
-      <Route path="/loader" element={<Loader/>}/>
     </Routes>
       )
 }
